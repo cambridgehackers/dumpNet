103d2ac05b838d26ae7b9771ce29bfb545351f5f
John Ankcorn <jca@alum.mit.edu>
2018-11-06 17:06:00 +0900

    more organize

--- organize.cpp
+++ organize.cpp
@@ -46,7 +46,6 @@ uint8_t *bufp = inbuf;
 
 std::map<std::string, LocInfo> locData;
 std::map<std::string, std::map<std::string, int>> abstractTile;
-typedef std::__list_iterator<SiteInfo, void *> SiteIterator;
 
 inline std::string autostr(uint64_t X, bool isNeg = false)
 {
@@ -75,90 +74,6 @@ bool inline startswith(std::string str, std::string suffix)
     return str.substr(0, suffix.length()) == suffix;
 }
 
-typedef struct {
-    const char *root;
-    int siteX;
-    int siteY;
-} MatchSite;
-
-typedef struct {
-    const char *name;
-    const char *newName;
-    MatchSite   node[20];
-} MatchPattern;
-
-#define ABSOLUTE   0x00100000
-#define MINUS1     0x00200000
-#define SETY       0x00400000
-#define YPLUS0     0x00800000
-#define YPLUS1     0x01000000
-#define YTIMES2    0x02000000
-#define YTIMES2P1  0x04000000
-#define YDIV2      0x08000000
-
-const char *GCC[] = {"CLBLM_R_", "INT_R_", nullptr};
-const char *GCL[] = {"CLBLM_L_", "INT_L_", nullptr};
-const char *GCR[] = {"CLBLL_R_", "INT_R_", nullptr};
-const char *GLL[] = {"CLBLL_L_", "INT_L_", nullptr};
-const char *GFL[] = {"CMT_FIFO_L_", nullptr};
-const char *GHH[] = {"HCLK_IOI3_", nullptr};
-const char *GBL[] = {"BRAM_L_", "INT_L_", nullptr};
-const char *GBR[] = {"BRAM_R_", "INT_R_", nullptr};
-const char *GDL[] = {"DSP_L_", "INT_L_", nullptr};
-const char *GDR[] = {"DSP_R_", "INT_R_", nullptr};
-const char *GIR[] = {"INT_R_", nullptr};
-const char *GIL[] = {"INT_L_", nullptr};
-const char *GRR[] = {"INT_R_", "RIOB33_", "RIOI3_", nullptr};
-const char *GRS[] = {"INT_R_", "RIOB33_SING_", "RIOI3_SING_", nullptr};
-const char *GRT[] = {"INT_R_", "RIOB33_", "RIOI3_TBYTETERM_", nullptr};
-const char *GST[] = {"INT_R_", "RIOB33_", "RIOI3_TBYTESRC_", nullptr};
-struct {
-    const char *name;
-    const char **params;
-} groups[] = {
-   {"GCL", GCL}, {"GCC", GCC}, {"GCR", GCR}, {"GLL", GLL}, {"GFL", GFL},
-   {"GBL", GBL}, {"GBR", GBR}, {"GHH", GHH}, {"GDL", GDL}, {"GDR", GDR},
-   {"GIR", GIR}, {"GIL", GIL}, {"GRR", GRR}, {"GRS", GRS}, {"GRT", GRT},
-   {"GST", GST}, {nullptr}};
-
-static MatchPattern tss[] = {
-    { "GFL", "", {{"OUT_FIFO", ABSOLUTE+0, SETY}, {"IN_FIFO", ABSOLUTE+0, YPLUS0}}},
-    { "GCC|GIL|GIR", "T0", {{"TIEOFF", 0, 0}}},
-    { "GCC|GIL|GIR", "T1", {{"TIEOFF", 1, 0}}},
-    { "GCC|GIL|GIR", "T2", {{"TIEOFF", 2, 0}}},
-    { "GCR",  "", {{"TIEOFF", 1, 0}, {"SLICE", 7, 0}, {"SLICE", 8, 0}}},
-    { "GCR", "6", {{"TIEOFF", 1, 0}, {"SLICE", 5, 0}, {"SLICE", 6, 0}}},
-    { "GCR", "4", {{"TIEOFF", 1, 0}, {"SLICE", 3, 0}, {"SLICE", 4, 0}}},
-    { "GCC",  "", {{"TIEOFF", 1, 0}, {"SLICE", 3, 0}, {"SLICE", 4, 0}}},
-    { "GCC", "0", {{"TIEOFF", 0, 0}, {"SLICE", 1, 0}, {"SLICE", 2, 0}}},
-    { "GCC", "14", {{"TIEOFF", 2, 0}, {"SLICE", 13, 0}, {"SLICE", 14, 0}}},
-    { "GCC", "12", {{"TIEOFF", 2, 0}, {"SLICE", 11, 0}, {"SLICE", 12, 0}}},
-    { "GCC", "_M1", {{"TIEOFF", 0, 0}, {"SLICE", MINUS1, 0}, {"SLICE", 0, 0}}},
-    { "GCC", "8", {{"TIEOFF", 1, 0}, {"SLICE", 7, 0}, {"SLICE", 8, 0}}},
-    { "GCC", "10", {{"TIEOFF", 1, 0}, {"SLICE", 9, 0}, {"SLICE", 10, 0}}},
-    { "GCC", "102", {{"TIEOFF", 2, 0}, {"SLICE", 9, 0}, {"SLICE", 10, 0}}},
-    { "GLL",  "", {{"SLICE", 1, 0}, {"SLICE", 0, 0}, {"TIEOFF", 0, 0}}},
-    { "GLL", "13", {{"SLICE", 13, 0}, {"SLICE", 12, 0}, {"TIEOFF", 2, 0}}},
-    { "GLL", "11", {{"SLICE", 11, 0}, {"SLICE", 10, 0}, {"TIEOFF", 2, 0}}},
-    { "GLL", "7", {{"SLICE", 7, 0}, {"SLICE", 6, 0}, {"TIEOFF", 1, 0}}},
-    { "GCL", "0", {{"SLICE", 3, 0}, {"SLICE", 2, 0}, {"TIEOFF", 0, 0}}},
-    { "GCL",  "", {{"SLICE", 3, 0}, {"SLICE", 2, 0}, {"TIEOFF", 1, 0}}},
-    { "GCL", "11", {{"SLICE", 11, 0}, {"SLICE", 10, 0}, {"TIEOFF", 2, 0}}},
-    { "GCL", "9", {{"SLICE", 9, 0}, {"SLICE", 8, 0}, {"TIEOFF", 1, 0}}},
-    { "GCL", "7", {{"SLICE", 7, 0}, {"SLICE", 6, 0}, {"TIEOFF", 1, 0}}},
-    { "GCL", "5", {{"SLICE", 5, 0}, {"SLICE", 4, 0}, {"TIEOFF", 1, 0}}},
-    { "GRS",  "", {{"TIEOFF", 2, 0}, {"OLOGIC", ABSOLUTE+0, 0}, {"ILOGIC", ABSOLUTE+0, 0}, {"IDELAY", ABSOLUTE+0, 0}, {"IOB", ABSOLUTE+0, 0}}},
-    { "GRR|GST|GRT",  "", {{"TIEOFF", 2, 0}, {"OLOGIC", ABSOLUTE+0, 0}, {"ILOGIC", ABSOLUTE+0, 0},
-          {"OLOGIC", ABSOLUTE+0, 1}, {"ILOGIC", ABSOLUTE+0, 1},
-          {"IDELAY", ABSOLUTE+0, 0}, {"IDELAY", ABSOLUTE+0, 1},
-          {"IOB", ABSOLUTE+0, 0}, {"IOB", ABSOLUTE+0, 1}}},
-    { "GDR",  "", {{"TIEOFF", 0, 0}, {"DSP48", ABSOLUTE+0, SETY}, {"DSP48", ABSOLUTE+0, YPLUS1}, {"TIEOFF", 1, 0}}},
-    { "GDL",  "1", {{"DSP48", ABSOLUTE+1, SETY}, {"DSP48", ABSOLUTE+1, YPLUS1}, {"TIEOFF", 1, 0}, {"TIEOFF", 2, 0}}},
-    { "GBL",  "0", {{"RAMB36", ABSOLUTE+0, SETY}, {"RAMB18", ABSOLUTE+0, YTIMES2}, {"RAMB18", ABSOLUTE+0, YTIMES2P1}, {"TIEOFF", 0, 0}}},
-    { "GBL",  "1", {{"RAMB36", ABSOLUTE+1, SETY}, {"RAMB18", ABSOLUTE+1, YTIMES2}, {"RAMB18", ABSOLUTE+1, YTIMES2P1}, {"TIEOFF", 1, 0}}},
-    { "GBR",  "2", {{"TIEOFF", 2, 0}, {"RAMB18", ABSOLUTE+2, SETY}, {"RAMB18", ABSOLUTE+2, YPLUS1}, {"RAMB36", ABSOLUTE+2, YDIV2}}},
-    {nullptr}};
-
 SiteInfo *getChild(std::list<SiteInfo> &children, int ind)
 {
     for (auto CI = children.begin(), CE = children.end(); CI != CE; CI++)
@@ -169,7 +84,6 @@ SiteInfo *getChild(std::list<SiteInfo> &children, int ind)
 
 void organizeNames()
 {
-int jca = 0;
     uint8_t *start = bufp;
     while (*bufp) {
         if (*bufp == '\n') {
@@ -180,8 +94,6 @@ int jca = 0;
             if (ind > 0) {
                 site = item.substr(ind + 2);
                 item = item.substr(0, ind);
-//printf("[%s:%d] item '%s' site '%s'\n", __FUNCTION__, __LINE__, item.c_str(), site.c_str());
-//if (++jca > 10) exit(-1);
                 ind = site.find("_X");
                 if (ind > 0) {
                     std::string temp = site.substr(ind+2);
@@ -217,8 +129,13 @@ int jca = 0;
             SiteInfo *c3 = getChild(citem->second, 3);
             SiteInfo *c4 = getChild(citem->second, 4);
             SiteInfo *c5 = getChild(citem->second, 5);
-            if ((tname == "INT_L_" || tname == "INT_R_") && psize == 1 && c0->root == "TIEOFF") {
-                goto nextTile;
+            if (((tname == "INT_L_" && (baseX & 1) == 0) || (tname == "INT_R_" && (baseX & 1) == 1))
+                && psize == 1 && c0->root == "TIEOFF" && c0->siteY == baseY
+                && (c0->siteX == baseX || c0->siteX == baseX + 1 || c0->siteX == baseX + 2)) {
+                int diff = c0->siteX - baseX;
+                //tname += autostr(diff);
+                if (baseX < 8 ? diff == 0 : baseX < 22 ? diff == 1 : diff == 2)
+                    goto addItem;
             }
             if ((tname == "CLBLL_R_" || tname == "CLBLL_L_" || tname == "CLBLM_R_" || tname == "CLBLM_L_")
              && psize == 2 && c0->root == "SLICE" && c1->root == "SLICE"
@@ -229,140 +146,91 @@ int jca = 0;
                     goto addItem;
             }
             if (tname == "CMT_FIFO_L_" && psize == 2
-                 && c0->root == "OUT_FIFO" && c1->root == "IN_FIFO") {
-                goto nextTile;
+                 && c0->root == "OUT_FIFO" && c1->root == "IN_FIFO"
+                 && c0->siteX == 0 && c1->siteX == 0 && c0->siteY == c1->siteY) {
+                tname += autostr(c0->siteY);
+                goto addItem;
             }
             if (tname == "BRAM_L_" && psize == 3
-                 && c0->root == "RAMB36" && c1->root == "RAMB18" && c2->root == "RAMB18") {
-                goto nextTile;
+                 && c0->root == "RAMB36" && c1->root == "RAMB18" && c2->root == "RAMB18"
+                 && c0->siteX == c1->siteX && c1->siteX == c2->siteX
+                 && (c0->siteX == 0 || c0->siteX == 1)
+                 && c0->siteY == baseY / 5
+                 && c0->siteY * 2 == c1->siteY && c0->siteY * 2 + 1 == c2->siteY) {
+                tname += autostr(c0->siteX);
+                goto addItem;
             }
             if (tname == "BRAM_R_" && psize == 3
-                 && c0->root == "RAMB18" && c1->root == "RAMB18" && c2->root == "RAMB36") {
-                goto nextTile;
+                 && c0->root == "RAMB18" && c1->root == "RAMB18" && c2->root == "RAMB36"
+                 && c0->siteX == c1->siteX && c1->siteX == c2->siteX
+                 && c0->siteX == 2
+                 && c0->siteY == 2 * (baseY / 5)
+                 && c0->siteY + 1 == c1->siteY && c0->siteY/2 == c2->siteY) {
+                goto addItem;
             }
-            if ((tname == "DSP_R_" || tname == "DSP_L_") && psize == 3
-                 && c0->root == "DSP48" && c1->root == "DSP48" && c2->root == "TIEOFF") {
-                goto nextTile;
+            if (((tname == "DSP_R_" && c0->siteX == 0) || (tname == "DSP_L_" && c0->siteX == 1)) && psize == 3
+                 && c0->root == "DSP48" && c1->root == "DSP48" && c2->root == "TIEOFF"
+                 && c1->siteX == c0->siteX && c2->siteX == baseX + 1
+                 && c0->siteY == (baseY / 5) * 2
+                 && c0->siteY + 1 == c1->siteY && c2->siteY == baseY) {
+                goto addItem;
             }
             if (tname == "RIOB33_" && psize == 2
-                 && c0->root == "IOB" && c1->root == "IOB") {
-                goto nextTile;
+                 && c0->root == "IOB" && c1->root == "IOB"
+                 && c0->siteX == 0 && c1->siteX == 0
+                 && c0->siteY == baseY
+                 && c0->siteY + 1 == c1->siteY) {
+                goto addItem;
             }
-            if (tname == "RIOB33_SING_" && psize == 1 && c0->root == "IOB") {
-                goto nextTile;
+            if (tname == "RIOB33_SING_" && psize == 1 && c0->root == "IOB"
+                 && c0->siteX == 0
+                 && c0->siteY == baseY) {
+                goto addItem;
             }
             if (tname == "RIOI3_SING_" && psize == 3
-                 && c0->root == "OLOGIC" && c1->root == "ILOGIC" && c2->root == "IDELAY") {
-                goto nextTile;
+                 && c0->root == "OLOGIC" && c1->root == "ILOGIC" && c2->root == "IDELAY"
+                 && c0->siteX == 0
+                 && c0->siteX == c1->siteX && c1->siteX == c2->siteX
+                 && c0->siteY == baseY
+                 && c0->siteY == c1->siteY && c1->siteY == c2->siteY) {
+                goto addItem;
             }
             if ((tname == "RIOI3_" || tname == "RIOI3_TBYTETERM_" || tname == "RIOI3_TBYTESRC_")
                  && psize == 6
                  && c0->root == "OLOGIC" && c1->root == "ILOGIC"
                  && c2->root == "OLOGIC" && c3->root == "ILOGIC"
-                 && c4->root == "IDELAY" && c5->root == "IDELAY") {
-                goto nextTile;
+                 && c4->root == "IDELAY" && c5->root == "IDELAY"
+                 && c0->siteX == 0
+                 && c0->siteX == c1->siteX && c1->siteX == c2->siteX
+                 && c2->siteX == c3->siteX && c3->siteX == c4->siteX
+                 && c4->siteX == c5->siteX
+                 && c0->siteY == baseY && c1->siteY == baseY
+                 && c2->siteY == baseY+1 && c3->siteY == baseY+1
+                 && c4->siteY == baseY && c5->siteY == baseY+1) {
+                goto addItem;
             }
-
-            //for (auto sitem: citem->second) {
-                //printf(" %s_X%dY%d", sitem.root.c_str(), sitem.siteX, sitem.siteY);
-            //}
+            if (tname == "PSS2_" && psize == 131) {
+                int ind = 0, offset = 0;
+                while (ind < 130) {
+                    SiteInfo *c = getChild(citem->second, ind);
+                    if (c->root != "IOPAD" || c->siteX != 1 || c->siteY != ind + offset + 1)
+                        goto nextItem;
+                    if (ind == 71)
+                        offset = 4;
+                    ind++;
+                }
+                if (SiteInfo *c = getChild(citem->second, 130))
+                if (c->root == "PS7" && c->siteX == 0 && c->siteY == 0)
+                    goto addItem;
+nextItem:;
+            }
             citem++;
             continue;
 addItem:;
             abstractTile[tname][lsort] = 1;
-nextTile:;
             citem = item->second.tiles.erase(citem);
         }
     }
-#if 0
-    for (auto item: locData) {
-        int index = 0;
-        while (groups[index].name) {
-            std::string name = groups[index].name;
-            int ind = item.first.find("Y");
-            std::string rem = item.first.substr(ind);
-            std::string newName;
-            int xoff = atoi(item.first.substr(1).c_str());
-            int yoffBase = atoi(rem.substr(1).c_str());
-            auto matchPattern = [&](MatchPattern *pattern) -> bool {
-                int reuseY = -1;
-                while (pattern->name) {
-                int ind = 0;
-                SiteIterator par = item.second.sites.begin();
-                if (pattern->name == std::string("GCC|GIL|GIR")) {
-                    if (name != "GCC" && name != "GIL" && name != "GIR")
-                        goto nextm;
-                }
-                else if (pattern->name == std::string("GRR|GST|GRT")) {
-                    if (name != "GRR" && name != "GST" && name != "GRT")
-                        goto nextm;
-                }
-                else if (name != pattern->name)
-                    goto nextm;
-                newName = pattern->newName;
-                while(pattern->node[ind].root) {
-                    int tempX = pattern->node[ind].siteX;
-                    if (tempX & ABSOLUTE)
-                        tempX &= ~ABSOLUTE;
-                    else if (tempX == MINUS1)
-                        tempX = xoff - 1;
-                    else
-                        tempX += xoff;
-                    int tempY = pattern->node[ind].siteY;
-                    if (tempY == SETY) {
-                        reuseY = par->siteY;
-                        newName += "_" + autostr(reuseY);
-                        tempY = reuseY;
-                    }
-                    else if (tempY == YPLUS0)
-                        tempY = reuseY;
-                    else if (tempY == YPLUS1)
-                        tempY = reuseY + 1;
-                    else if (tempY == YTIMES2)
-                        tempY = 2 * reuseY;
-                    else if (tempY == YTIMES2P1)
-                        tempY = 2 * reuseY + 1;
-                    else if (tempY == YDIV2) {
-                        if (reuseY & 1)
-                            goto nextm;
-                        tempY = reuseY / 2;
-                    }
-                    else
-                        tempY += yoffBase;
-                    if (par == item.second.sites.end()
-                     || par->root != pattern->node[ind].root
-                     || par->siteX != tempX || par->siteY != tempY)
-                        goto nextm;
-                    par++;
-                    ind++;
-                }
-                if (par != item.second.sites.end())
-                    goto nextm;
-                return true;
-nextm:
-                pattern++;
-                }
-                return false;
-            };
-            int i = 0;
-            for (auto citem: item.second.tiles) {
-                 if (!groups[index].params[i] || groups[index].params[i] != citem.first)
-                     goto next2;
-                 i++;
-            }
-            if (groups[index].params[i])
-                goto next2;
-            locData[item.first].tiles.clear();
-            if (matchPattern(tss))
-                abstractTile[name + newName].push_back(item.first);
-            //else
-                //locData[item.first].tiles[groups[index].name] = 1;
-            break;
-next2:;
-            index++;
-        }
-    }
-#endif
     for (auto item: locData) {
         if (item.second.tiles.size() == 0)
             continue;
@@ -395,7 +263,7 @@ next2:;
             lastY = offY;
             lastX = offX;
         }
-        if (inRun)
+        if (inRun && lastY != startY)
             printf("-%d", lastY);
         printf("\n");
     }
